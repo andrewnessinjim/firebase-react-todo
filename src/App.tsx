b36cc56d68720f267bccb49763b096dd86508e02
Andrew Nessin <andrewnessinjim@yahoo.com>
2022-04-04 21:19:36 +0530

    Connect CRA app to firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import { collection, addDoc, getDocs } from "firebase/firestore"; 
+import { useFirebaseApp } from './infra/FirebaseProvider';
+
+type Todo = {
+  id: string,
+  todoText: string
+}
 
 function App() {
+  const [todoText, setTodoText] = useState("");
+  const db = useFirebaseApp();
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [refreshRequired, setRefreshRequired] = useState(true);
+  useEffect(() => {
+    if(refreshRequired) {
+      (async function() {
+        const uiTodos:Todo[] = [];
+        try {
+          const dbTodos = (await getDocs(collection(db, "todos")));
+          dbTodos.forEach(doc => uiTodos.push({todoText: doc.data().todoText, id:doc.id}));
+          setTodos(uiTodos);
+        } catch(e) {
+          console.log("Error fetching documents", e)
+        }
+      })();
+    }
+    setRefreshRequired(false);
+  }, [db, refreshRequired]);
+  
+  async function addTodo(e:any){
+    e.preventDefault();
+
+    try {
+      const docRef = await addDoc(collection(db, "todos"), {
+        todoText
+      });
+      console.log("Document written with ID: ", docRef.id);
+      setTodoText("");
+      setRefreshRequired(true)
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Your Todos</h1>
+        <ol>
+          {todos?.map(todo => <li key={todo.id}>{todo.todoText}</li>)}
+        </ol>
+       <form>
+         <input type="text" value={todoText} onChange={e => setTodoText(e.target.value)}/>
+         <button onClick={addTodo}>Save Todo</button>
+       </form>
     </div>
   );
 }
