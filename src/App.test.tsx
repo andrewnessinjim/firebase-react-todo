6dfe42402fc9116a3546e022d1f0fd5b0626e7ed
Andrew Nessin <andrewnessinjim@yahoo.com>
2022-07-14 14:24:35 +0530

    Fix unit test failure

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import TodoApp from './App';
+import { FirebaseAppProvider } from './infra/FirebaseProvider';
 
-test('renders learn react link', () => {
-  render(<TodoApp />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders current toods header', () => {
+  render(
+    <FirebaseAppProvider>
+      <TodoApp />
+    </FirebaseAppProvider>);
+  const linkElement = screen.getByText(/Current Todos/i);
   expect(linkElement).toBeInTheDocument();
 });
